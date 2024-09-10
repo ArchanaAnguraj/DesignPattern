1ed71d8fd9dbffdeca96af611cca3091fe75409f
ArchanaAnguraj <archanaat12345@gmail.com>
2024-09-10 12:38:18 +0530

    Update DesignPattern.java

--- DesignPattern.java
+++ DesignPattern.java
@@ -2,62 +2,62 @@ package ONLINEVDOS;
 //lazy
 
 
-//class Dog
-//{
-//	static Dog d;
-//	private Dog() //why we give constructor its for object creation
-//	{
-//		//super()--its go to object ,its create the objects "constructor also create object"
-//	}
-//	static synchronized
+class Dog
+{
+	static Dog d;
+	private Dog() //why we give constructor its for object creation
+	{
+		//super()--its go to object ,its create the objects "constructor also create object"
+	}
 
 
-//Dog getDog()
-//	{
-//		
-//		if(d==null) {
-//		Dog d=new Dog();
-//		}
-//       return d;
-//	}
-//}
-//public class DesignPattern {
-//
-//	public static void main(String[] args) {
-//		Dog d1=Dog.getDog();
-//		
-//
-//	}
-//
-//}
-//------------------------//
+      Dog getDog()
+	{
+		if(d==null) {
+		Dog d=new Dog();
+		}
+      return d;
+	}
+}
+public class DesignPattern {
+
+	public static void main(String[] args) {
+		Dog d1=Dog.getDog();
+		Dog d2=Dog.getDog();
+		System.out.println(d1);
+		System.out.println(d2);
+
+	}
+
+}
+------------------------//
 
 //Early
-//class Dog
-//{
-//	
-//	static Dog d=new Dog();
-//	private Dog() //why we give constructor its for object creation
-//	{
-//		//super()--its go to object ,its create the objects "constructor also create object"
-//	}
-//	static Dog getDog()
-//	{
-//		return d;
-//	}
-//}
-//public class DesignPattern {
-//
-//	public static void main(String[] args) {
-//		Dog d1=Dog.getDog();
-//		Dog d2=Dog.getDog();
-//		System.out.println(d1);
-//		System.out.println(d2);
-//		
-//		
-//
-//	}
-//}
+class Dog
+{
+	
+	static Dog d=new Dog();
+	private Dog() //why we give constructor its for object creation
+	{
+		//super()--its go to object ,its create the objects "constructor also create object"
+	}
+	static Dog getDog()
+	{
+		return d;
+	}
+}
+public class DesignPattern {
+
+	public static void main(String[] args) {
+		Dog d1=Dog.getDog();
+		Dog d2=Dog.getDog();
+		System.out.println(d1);
+		System.out.println(d2);
+		
+		
+
+	}
+}
 
 //-----------------------------------------------//
 //factory design
@@ -98,10 +98,12 @@ public class DesignPattern {
 	public static void main(String[] args) {
 		Animal d=AnimalFactory.instanceMethod("dog");
 		Animal c=AnimalFactory.instanceMethod("cat");
+		System.out.println(d);
+		System.out.println(c);
 
 	}
 }
-
+//----------------------------------------------------------------------------------//
 
 
 
